623680f59bd688d2f538b596e4ee6382dd42b37b
Moi <moi@moi-GT680-GX680.(none)>
2015-05-01 14:06:58 -0700

    f

--- cputopology.c
+++ cputopology.c
@@ -39,40 +39,40 @@
 static inline void cpuid(uint32_t info1, uint32_t info2, uint32_t *eaxp,
                          uint32_t *ebxp, uint32_t *ecxp, uint32_t *edxp)
 {
-	uint32_t eax, ebx, ecx, edx;
-	/* Can select with both eax (info1) and ecx (info2) */
-	asm volatile("cpuid" 
-		: "=a" (eax), "=b" (ebx), "=c" (ecx), "=d" (edx)
-		: "a" (info1), "c" (info2));
-	if (eaxp)
-		*eaxp = eax;
-	if (ebxp)
-		*ebxp = ebx;
-	if (ecxp)
-		*ecxp = ecx;
-	if (edxp)
-		*edxp = edx;
+  uint32_t eax, ebx, ecx, edx;
+  /* Can select with both eax (info1) and ecx (info2) */
+  asm volatile("cpuid" 
+	       : "=a" (eax), "=b" (ebx), "=c" (ecx), "=d" (edx)
+	       : "a" (info1), "c" (info2));
+  if (eaxp)
+    *eaxp = eax;
+  if (ebxp)
+    *ebxp = ebx;
+  if (ecxp)
+    *ecxp = ecx;
+  if (edxp)
+    *edxp = edx;
 }
 
 static void pin_to_core(int i)
 {
-	cpu_set_t cpuset;
-	CPU_ZERO(&cpuset);
-	CPU_SET(i, &cpuset);
-	sched_setaffinity(0, sizeof(cpu_set_t), &cpuset);
-	sched_yield();
+  cpu_set_t cpuset;
+  CPU_ZERO(&cpuset);
+  CPU_SET(i, &cpuset);
+  sched_setaffinity(0, sizeof(cpu_set_t), &cpuset);
+  sched_yield();
 }
 
 static void *core_proxy(void *arg)
 {
-	/* Pin to core */
-	int coreid = (int)(long)arg;
-	pin_to_core(coreid);
+  /* Pin to core */
+  int coreid = (int)(long)arg;
+  pin_to_core(coreid);
 
-	/* Do some cpuid stuff */
-uint32_t eax, ebx, ecx, edx;
-  uint32_t x2apic_id, smt_mask_width, smt_select_mask, smt_id,
-    core_plus_mask_width,core_only_select_mask,core_id;
+  /* Do some cpuid stuff */
+  uint32_t eax, ebx, ecx, edx;
+  uint32_t x2apic_id, core_plus_mask_width,
+    core_only_select_mask,core_id;
 
  
   uint32_t apic_id,count,logical_cpu_bits,core_bits,logical_CPU_number_within_core,core_number_within_chip,chip_id;
@@ -98,22 +98,22 @@ uint32_t eax, ebx, ecx, edx;
   core_number_within_chip = (apic_id >> logical_cpu_bits) & ( (1 << core_bits) -1) ;
   chip_id = apic_id & ~( (1 << (logical_cpu_bits+core_bits) ) -1) ;
 
-   printf("I am core: %d, logical_CPU_number_within_core: 0x%08x,apic_id: 0x%08x,core_number_within_chip: 0x%08x, chip_id: 0x%08x\n",
+  printf("I am core: %d, logical_CPU_number_within_core: 0x%08x,apic_id: 0x%08x,core_number_within_chip: 0x%08x, chip_id: 0x%08x\n",
   	 coreid, logical_CPU_number_within_core, apic_id, core_number_within_chip,chip_id);
 
 }
 
 int main(int argc, char **argv)
 {
-	int ncpus = get_nprocs();
-	pthread_t pthread[ncpus];
-	for (int i=0; i<ncpus; i++) {
-		pthread_create(&pthread[i], NULL, core_proxy, (void*)(long)i);
-	}
-	for (int i=0; i<ncpus; i++) {
-		pthread_join(pthread[i], NULL);
-	}
+  int ncpus = get_nprocs();
+  pthread_t pthread[ncpus];
+  for (int i=0; i<ncpus; i++) {
+    pthread_create(&pthread[i], NULL, core_proxy, (void*)(long)i);
+  }
+  for (int i=0; i<ncpus; i++) {
+    pthread_join(pthread[i], NULL);
+  }
 	
-	return 0;
+  return 0;
 }
 
