89bc7081805437392a42b70ed7d4958141b60c50
Kevin Klues <klueska@cs.berkeley.edu>
2015-05-29 11:38:27 -0700

    Add myself to the copyright

--- schedule.h
+++ schedule.h
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2015 The Regents of the University of California
  * Valmon Leymarie <leymariv@berkeley.edu>
+ * Kevin Klues <klueska@cs.berkeley.edu>
  * See LICENSE for details.
  */
 
