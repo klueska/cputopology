6280d079c4d927c9a4a01ab1cd97f258269bebe0
Valmon Leymarie <leymariv@berkeley.edu>
2015-06-04 14:20:03 -0700

    Test of re allocating and re yielding a resource
    Tests ok when we try to reallocate a resource that is not availabe and when
    we try to yield a core that is available.

--- test_schedule.c
+++ test_schedule.c
@@ -41,6 +41,16 @@ int allocate_spec_core(int i)
 		return 0;
 }
 
+/* Allocate any core of the system. Return !0 if success */
+int allocate_any_core() 
+{
+	struct node *np = request_core_any();
+	if (np != NULL)
+		return np->refcount;
+	else 
+		return 0;
+}
+
 /* Yield core i of the system. Return 0 if success. */
 int yield_spec_core(int i) 
 {
@@ -57,10 +67,23 @@ int allocate_spec_chip(int i)
 		return 0;
 }
 
+/* Allocate any chip of the system. Return !0 if success */
+int allocate_any_chip() 
+{
+	struct node *np = request_chip_any();
+	if (np != NULL)
+		return np->refcount;
+	else 
+		return 0;
+}
+
 /* Yield all the chips of the system. Return 0 if success. */
 int yield_spec_chip(int i) 
 {
-	return yield_core_specific(i*2) + yield_core_specific(i*2+1);
+	int ret =0;
+	for (int j = 0 ; j<cores_per_chip; j++)
+		ret += yield_core_specific(i*cores_per_chip+j);
+	return ret;
 }
 
 /* Allocate socket i of the system. Return !0 if success */
@@ -73,12 +96,22 @@ int allocate_spec_socket(int i)
 		return 0;
 }
 
+/* Allocate any socket of the system. Return !0 if success */
+int allocate_any_socket() 
+{
+	struct node *np = request_socket_any();
+	if (np != NULL)
+		return np->refcount;
+	else 
+		return 0;
+}
+
 /* Yield all the sockets of the system. Return 0 if success. */
 int yield_spec_socket(int i) 
 {
 	int ret =0;
 	for (int j = 0 ; j<cores_per_socket; j++)
-		ret = yield_core_specific(i*cores_per_socket+j);
+		ret += yield_core_specific(i*cores_per_socket+j);
 	return ret;
 }
 
@@ -92,22 +125,36 @@ int allocate_spec_numa(int i)
 		return 0;
 }
 
+/* Allocate any numa of the system. Return !0 if success */
+int allocate_any_numa() 
+{
+	struct node *np = request_numa_any();
+	if (np != NULL)
+		return np->refcount;
+	else 
+		return 0;
+}
+
 /* Yield all the numas of the system. Return 0 if success. */
 int yield_spec_numa(int i) 
 {
 	int ret =0;
 	for (int j = 0 ; j<cores_per_numa; j++)
-		ret = yield_core_specific(i*cores_per_numa+j);
+		ret += yield_core_specific(i*cores_per_numa+j);
 	return ret;
 }
 
-/* Allocate and yield successively all cores, chips, sockets and numa of the system */
-void test_aloc_yield_all_typeofnodes()
+void alloc_yield_cores_specific()
 {
 /* Allocate all the cores of the system. */
 	for (int i = 0; i< num_cores; i++) {
 		if (allocate_spec_core(i) == 0) {
 			printf("allocate_spec_core: Error when allocating a core\n");
+			return;
+		}
+/* Trying to re allocate the same core twice shouldn't work */
+		if (allocate_spec_core(i) != 0) {
+			printf("allocate_spec_core: Error when re-allocating a core\n");
 			return;
 		}
 	}
@@ -118,14 +165,51 @@ void test_aloc_yield_all_typeofnodes()
 			printf("yield_spec_core: Error when yielding a core\n");
 			return;
 		}
+/* Trying to re yield the same core twice shouldn't work */
+		if (yield_spec_core(i) > -1) {
+			printf("yield_spec_core: Error when re-yielding a core\n");
+			return;
+		}
 	}
-	
+}
+
+void alloc_yield_cores_any()
+{
+/* Allocate all the cores of the system. */
+	for (int i = 0; i< num_cores; i++) {
+		if (allocate_any_core() == 0) {
+			printf("allocate_any_core: Error when allocating a core\n");
+			return;
+		}
+	}
+
+/* Yield all the cores of the system. */
+	for (int i = 0; i< num_cores; i++) {
+		if (yield_spec_core(i) != 0) {
+			printf("yield_spec_core: Error when yielding a core\n");
+			return;
+		}
+/* Trying to re yield the same core twice shouldn't work */
+		if (yield_spec_core(i) > -1) {
+			printf("yield_spec_core: Error when re-yielding a core\n");
+			return;
+		}
+	}
+}
+
+void alloc_yield_chips_specific()
+{
 /* Allocate all the chips of the system. */
 	for (int i = 0; i< num_chips; i++) {
 		if (allocate_spec_chip(i) == 0) {
 			printf("allocate_spec_chip: Error when allocating a chip\n");
 			return;
 		}
+/* Trying to re allocate the same chip twice shouldn't work */
+		if (allocate_spec_chip(i) != 0) {
+			printf("allocate_spec_chip: Error when re-allocating a chip\n");
+			return;
+		}
 	}
 
 /* Yield all the chips of the system. */
@@ -134,12 +218,20 @@ void test_aloc_yield_all_typeofnodes()
 			printf("yield_spec_chip: Error when yielding a chip\n");
 			return;
 		}
+/* Trying to re yield the same chip twice shouldn't work */
+		if (yield_spec_chip(i) > -1) {
+			printf("yield_spec_chip: Error when re-yielding a chip\n");
+			return;
+		}
 	}
+}
 
+void alloc_yield_chips_any()
+{
 /* Allocate all the chips of the system. */
-	for (int i = 0; i< num_sockets; i++) {
-		if (allocate_spec_socket(i) == 0) {
-			printf("allocate_spec_socket: Error when allocating a socket\n");
+	for (int i = 0; i< num_chips; i++) {
+		if (allocate_any_chip(i) == 0) {
+			printf("allocate_any_chip: Error when allocating a chip\n");
 			return;
 		}
 	}
@@ -150,12 +242,25 @@ void test_aloc_yield_all_typeofnodes()
 			printf("yield_spec_chip: Error when yielding a chip\n");
 			return;
 		}
+/* Trying to re yield the same chip twice shouldn't work */
+		if (yield_spec_chip(i) > -1) {
+			printf("yield_spec_chip: Error when re-yielding a chip\n");
+			return;
+		}
 	}
-	
+}
+
+void alloc_yield_sockets_specific()
+{
 /* Allocate all the sockets of the system. */
 	for (int i = 0; i< num_sockets; i++) {
 		if (allocate_spec_socket(i) == 0) {
 			printf("allocate_spec_socket: Error when allocating a socket\n");
+			return;
+		}
+/* Trying to re allocate the same socket twice shouldn't work */
+		if (allocate_spec_socket(i) != 0) {
+			printf("allocate_spec_socket: Error when re-allocating a socket\n");
 			return;
 		}
 	}
@@ -166,12 +271,49 @@ void test_aloc_yield_all_typeofnodes()
 			printf("yield_spec_socket: Error when yielding a socket\n");
 			return;
 		}
+/* Trying to re yield the same socket twice shouldn't work */
+		if (yield_spec_socket(i) > -1) {
+			printf("yield_spec_socket: Error when re-yielding a socket\n");
+			return;
+		}
+	}
+}
+
+void alloc_yield_sockets_any()
+{
+/* Allocate all the sockets of the system. */
+	for (int i = 0; i< num_sockets; i++) {
+		if (allocate_any_socket(i) == 0) {
+			printf("allocate_any_socket: Error when allocating a socket\n");
+			return;
+		}
 	}
 
+/* Yield all the sockets of the system. */
+	for (int i = 0; i< num_sockets; i++) {
+		if (yield_spec_socket(i) != 0) {
+			printf("yield_spec_socket: Error when yielding a socket\n");
+			return;
+		}
+/* Trying to re yield the same socket twice shouldn't work */
+		if (yield_spec_socket(i) > -1) {
+			printf("yield_spec_socket: Error when re-yielding a socket\n");
+			return;
+		}
+	}
+}
+
+void alloc_yield_numas_specific()
+{
 /* Allocate all the numas of the system. */
 	for (int i = 0; i< num_numa; i++) {
 		if (allocate_spec_numa(i) == 0) {
 			printf("allocate_spec_numa: Error when allocating a numa\n");
+			return;
+		}
+/* Trying to re allocate the same numa twice shouldn't work */
+		if (allocate_spec_numa(i) != 0) {
+			printf("allocate_spec_numa: Error when re-allocating a numa\n");
 			return;
 		}
 	}
@@ -182,10 +324,53 @@ void test_aloc_yield_all_typeofnodes()
 			printf("yield_spec_numa: Error when yielding a numa\n");
 			return;
 		}
+/* Trying to re yield the same numa twice shouldn't work */
+		if (yield_spec_numa(i) > -1) {
+			printf("yield_spec_numa: Error when re-yielding a numa\n");
+			return;
+		}
 	}
+}
+
+void alloc_yield_numas_any()
+{
+/* Allocate all the numas of the system. */
+	for (int i = 0; i< num_numa; i++) {
+		if (allocate_any_numa(i) == 0) {
+			printf("allocate_any_numa: Error when allocating a numa\n");
+			return;
+		}
+	}
+
+/* Yield all the numas of the system. */
+	for (int i = 0; i< num_numa; i++) {
+		if (yield_spec_numa(i) != 0) {
+			printf("yield_spec_numa: Error when yielding a numa\n");
+			return;
+		}
+/* Trying to re yield the same numa twice shouldn't work */
+		if (yield_spec_numa(i) > -1) {
+			printf("yield_spec_numa: Error when re-yielding a numa\n");
+			return;
+		}
+	}
+}
+
+/* Allocate and yield successively all cores, chips, sockets and numa of the system */
+void test_aloc_yield_all_typeofnodes()
+{
+	alloc_yield_cores_specific();
+	alloc_yield_cores_any();
+	alloc_yield_chips_specific();
+	alloc_yield_chips_any();
+	alloc_yield_sockets_specific();
+	alloc_yield_sockets_any();
+	alloc_yield_numas_specific();
+	alloc_yield_numas_any();
 }
 
 void test_schedule()
 {
-test_aloc_yield_all_typeofnodes();
+	test_aloc_yield_all_typeofnodes();
+	
 }
