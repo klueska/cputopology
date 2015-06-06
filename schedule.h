3e8c902cb09961ea55cff8ed605e95ee95778750
Kevin Klues <klueska@cs.berkeley.edu>
2015-06-06 11:50:39 -0700

    Add a node_label array of strings for printing

--- schedule.h
+++ schedule.h
@@ -15,6 +15,7 @@
 #define	SCHEDULE_H
 
 enum node_type { CORE, CHIP, SOCKET, NUMA };
+static char node_label[4][7] = { "CORE", "CHIP", "SOCKET", "NUMA" };
 #define NUM_NODE_TYPES sizeof(enum node_type)
 #define child_node_type(t) ((t) - 1)
 
